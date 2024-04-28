4975c313102d5f11dd5d5518595608dec3d79fb6
Никита Погорелов <nikipog@mail.ru>
2024-04-28 15:17:33 +0300

    style

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,5 +1,5 @@
 import { Link, Outlet, useLocation } from 'react-router-dom';
-import { AppRoute, AuthorizationStatus, Endpoint } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import { getAuthorizationStatus } from '../../mocks/authorizations-status';
 
 
